df57eacc673a64523f9e3f6fdc493bb13c02e327
thesiya-nikunj <wd.nikunj@gmail.com>
2021-06-11 19:56:33 +0530

    Some Animations Added

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,7 +8,7 @@ function Product({ id, title, price, description, category, image }) {
 
 	const [hasPrime] = useState(Math.random() < 0.5);
 	return (
-		<div className="relative rounded-md sm:w-72 md:w-80 xl:w-72 flex flex-col z-30 mx-5 my-2 md:mx-0 md:my-0 p-8 bg-white">
+		<div className="relative rounded-xl sm:w-72 md:w-80 xl:w-72 flex flex-col z-30 mx-5 my-2 md:mx-0 md:my-0 p-8 bg-white  cursor-pointer hover:shadow-xl border border-transparent hover:border-gray-200">
 			<p className="absolute top-4 right-4 bg-yellow-100 rounded-md py-1.5 px-2 text-xs text-gray-500">
 				{category}
 			</p>
@@ -27,7 +27,7 @@ function Product({ id, title, price, description, category, image }) {
 					<img src={Prime} alt="prime" className="w-16 object-contain" />
 				)}
 			</div>
-			<button className="bg-yellow-200 text-sm py-2.5 rounded focus:outline-none hover:bg-yellow-300">
+			<button className="bg-yellow-200 text-sm py-2.5 rounded-lg focus:outline-none hover:bg-yellow-300">
 				Add to Cart
 			</button>
 		</div>
