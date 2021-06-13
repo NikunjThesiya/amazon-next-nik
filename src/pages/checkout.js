fdb6c3451a253dcc43b928fa32d1ff22cd07c72d
thesiya-nikunj <wd.nikunj@gmail.com>
2021-06-13 12:43:26 +0530

    Styling Update

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -18,7 +18,7 @@ function Checkout() {
 				<div className="flex-grow space-y-5 m-2 md:m-5 px-5">
 					<img src={Banner} alt="banner" className="object-contain w-full" />
 
-					<div className="flex flex-col md:px-8 py-8 space-y-6 bg-white">
+					<div className="flex flex-col md:px-8 py-8 pb-52 lg:pb-0 space-y-6 bg-white">
 						<h1 className="text-lg md:text-2xl lg:text-3xl font-medium text-gray-500">
 							{items.length === 0
 								? "Your Amazon Cart is empty"
@@ -44,19 +44,19 @@ function Checkout() {
 				{/* Right */}
 				<div className="">
 					{items.length > 0 && (
-						<div className="md:sticky md:top-32 flex flex-col items-center bg-white m-5 px-10 py-10 rounded-2xl lg:rounded-none">
-							<h2 className="whitespace-nowrap">
+						<div className="w-full lg:w-auto fixed bottom-0 bg-amazon_blue-light lg:sticky lg:top-32 flex flex-col items-center lg:bg-white lg:m-5 px-10 md:py-10 py-8 overflow-hidden">
+							<h2 className="whitespace-nowrap text-white lg:text-gray-500">
 								Subtotal ({items.length} Items)
 							</h2>
-							<span className="font-bold text-lg text-gray-500">
+							<span className="font-semibold text-lg text-gray-50 lg:text-gray-500">
 								<Currency quantity={total} currency="USD" />
 							</span>
 
 							<button
 								className={
 									session
-										? "shadow-lg border border-transparent text-sm mt-3 py-2.5 px-4 rounded-lg focus:outline-none bg-yellow-200 hover:shadow-md hover:bg-yellow-100 hover:border-yellow-200 self-end justify-self-end"
-										: "shadow-lg border border-transparent text-sm mt-3 py-2.5 px-4 rounded-lg focus:outline-none bg-gray-300 border-gray-400 cursor-not-allowed"
+										? "shadow-lg border border-transparent text-xs md:text-sm mt-3 py-2.5 px-4 rounded-lg focus:outline-none bg-yellow-200 hover:shadow-md hover:bg-yellow-100 hover:border-yellow-200 whitespace-nowrap"
+										: "shadow-lg border border-transparent text-xs md:text-sm mt-3 py-2.5 px-4 rounded-lg focus:outline-none bg-gray-300 border-gray-400 cursor-not-allowed whitespace-nowrap"
 								}
 								disabled={!session}
 							>
